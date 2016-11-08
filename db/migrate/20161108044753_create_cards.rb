df23d7677e703623e4c4f4ad16e895e906f8a4ab
Dean Silfen <dean.silfen@gmail.com>
2016-11-08 00:04:18 -0500

    Nulls are bad

--- db/migrate/20161108044753_create_cards.rb
+++ db/migrate/20161108044753_create_cards.rb
@@ -1,9 +1,9 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
-      t.belongs_to :categories
-      t.string :question
-      t.string :answer
+      t.belongs_to :categories, null: false
+      t.string :question, null: false
+      t.string :answer, null: false
       t.datetime :deactivated_at
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 20161108044753) do
   enable_extension "plpgsql"
 
   create_table "cards", force: :cascade do |t|
-    t.integer  "categories_id"
-    t.string   "question"
-    t.string   "answer"
+    t.integer  "categories_id",  null: false
+    t.string   "question",       null: false
+    t.string   "answer",         null: false
     t.datetime "deactivated_at"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
