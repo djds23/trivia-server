85b4037c500106cbd0ece5df49e95cdeaf020f99
Dean Silfen <dean.silfen@gmail.com>
2016-11-25 00:44:00 -0500

    Update reasons controller, remove mutating endpoints

--- app/controllers/api/v1/reasons_controller.rb
+++ app/controllers/api/v1/reasons_controller.rb
@@ -8,44 +8,14 @@ class Api::V1::ReasonsController < ApplicationController
     render json: @reasons
   end
 
-  # GET /reasons/1
-  def show
-    render json: @reason
-  end
-
-  # POST /reasons
-  def create
-    @reason = Reason.new(reason_params)
-
-    if @reason.save
-      render json: @reason, status: :created, location: @reason
-    else
-      render json: @reason.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /reasons/1
-  def update
-    if @reason.update(reason_params)
-      render json: @reason
-    else
-      render json: @reason.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /reasons/1
-  def destroy
-    @reason.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reason
-      @reason = Reason.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reason
+    @reason = Reason.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def reason_params
-      params.require(:reason).permit(:display_name, :deactivated_at, :machine_name)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def reason_params
+    params.require(:reason).permit(:display_name, :deactivated_at, :machine_name)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108182039) do
+ActiveRecord::Schema.define(version: 20161125042905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20161108182039) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.datetime "deactivated_at"
+  end
+
+  create_table "reasons", force: :cascade do |t|
+    t.string   "display_name"
+    t.datetime "deactivated_at"
+    t.string   "machine_name"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
