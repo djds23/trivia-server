56c43b1879b1194ec3357c08c58b7e85e8424b3c
Dean Silfen <dean.silfen@gmail.com>
2016-11-25 00:58:05 -0500

    Update reasons specs

--- spec/requests/reasons_spec.rb
+++ spec/requests/reasons_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Reasons", type: :request do
+describe "Reasons", type: :request do
   describe "GET /reasons" do
     it "works! (now write some real specs)" do
-      get reasons_path
+      get api_v1_reasons_path
       expect(response).to have_http_status(200)
     end
   end
--- spec/routing/reasons_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-describe Api::V1::ReasonsController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(:get => "api/v1/reasons").to route_to("api_v1_reasons#index")
-    end
-  end
-end
