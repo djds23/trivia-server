4593492e0daf8f85a23131dc2303fecd6dca2e71
Dean Silfen <dean.silfen@gmail.com>
2016-11-25 00:54:33 -0500

    Add specs

--- spec/controllers/reasons_controller_spec.rb
+++ spec/controllers/reasons_controller_spec.rb
@@ -18,142 +18,24 @@ require 'rails_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-RSpec.describe ReasonsController, type: :controller do
+describe Api::V1::ReasonsController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
   # Reason. As you add validations to Reason, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+  let!(:reason) {
+    Reason.create!(
+      machine_name: 'test',
+      display_name: 'Test Reason'
+    )
   }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # ReasonsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
 
   describe "GET #index" do
     it "assigns all reasons as @reasons" do
-      reason = Reason.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(assigns(:reasons)).to eq([reason])
+      get :index
+      json_body = JSON.parse(response.body)
+      fetched_reason = Reason.find json_body.first['id']
+      expect(fetched_reason).to eq(reason)
     end
   end
-
-  describe "GET #show" do
-    it "assigns the requested reason as @reason" do
-      reason = Reason.create! valid_attributes
-      get :show, params: {id: reason.to_param}, session: valid_session
-      expect(assigns(:reason)).to eq(reason)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new reason as @reason" do
-      get :new, params: {}, session: valid_session
-      expect(assigns(:reason)).to be_a_new(Reason)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested reason as @reason" do
-      reason = Reason.create! valid_attributes
-      get :edit, params: {id: reason.to_param}, session: valid_session
-      expect(assigns(:reason)).to eq(reason)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Reason" do
-        expect {
-          post :create, params: {reason: valid_attributes}, session: valid_session
-        }.to change(Reason, :count).by(1)
-      end
-
-      it "assigns a newly created reason as @reason" do
-        post :create, params: {reason: valid_attributes}, session: valid_session
-        expect(assigns(:reason)).to be_a(Reason)
-        expect(assigns(:reason)).to be_persisted
-      end
-
-      it "redirects to the created reason" do
-        post :create, params: {reason: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(Reason.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns a newly created but unsaved reason as @reason" do
-        post :create, params: {reason: invalid_attributes}, session: valid_session
-        expect(assigns(:reason)).to be_a_new(Reason)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, params: {reason: invalid_attributes}, session: valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested reason" do
-        reason = Reason.create! valid_attributes
-        put :update, params: {id: reason.to_param, reason: new_attributes}, session: valid_session
-        reason.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested reason as @reason" do
-        reason = Reason.create! valid_attributes
-        put :update, params: {id: reason.to_param, reason: valid_attributes}, session: valid_session
-        expect(assigns(:reason)).to eq(reason)
-      end
-
-      it "redirects to the reason" do
-        reason = Reason.create! valid_attributes
-        put :update, params: {id: reason.to_param, reason: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(reason)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns the reason as @reason" do
-        reason = Reason.create! valid_attributes
-        put :update, params: {id: reason.to_param, reason: invalid_attributes}, session: valid_session
-        expect(assigns(:reason)).to eq(reason)
-      end
-
-      it "re-renders the 'edit' template" do
-        reason = Reason.create! valid_attributes
-        put :update, params: {id: reason.to_param, reason: invalid_attributes}, session: valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested reason" do
-      reason = Reason.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: reason.to_param}, session: valid_session
-      }.to change(Reason, :count).by(-1)
-    end
-
-    it "redirects to the reasons list" do
-      reason = Reason.create! valid_attributes
-      delete :destroy, params: {id: reason.to_param}, session: valid_session
-      expect(response).to redirect_to(reasons_url)
-    end
-  end
-
 end
